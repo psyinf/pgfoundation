18036b0914d08d7b3b9c08e175062120973298b7
psy_inf <sebastian@smesserschmidt.de>
2024-09-21 21:55:28 +0200

    async task handling

--- pgfoundation/include/pgf/taskengine/TaskEngine.hpp
+++ pgfoundation/include/pgf/taskengine/TaskEngine.hpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <thread>
 #include <future>
+#include <iostream>
 
 namespace pgf {
 using namespace std::chrono_literals;
@@ -18,9 +19,9 @@ struct Task
     using Duration = std::chrono::high_resolution_clock::duration;
     using TimePoint = std::chrono::high_resolution_clock::time_point;
 
-    bool execute() { return task(); }
+    // bool execute() { return task(); }
 
-    std::function<bool()> task; //< the task needs to return true if it was successful, false otherwise.
+    std::function<bool()> work; //< the task needs to return true if it was successful, false otherwise.
     bool                  reschedule_on_failure = false; //< if the task fails, should it be rescheduled?
     Duration              starting_time_offset{0ms};     //< delay before executing the task
     Duration              reschedule_delay{0ms};         //< delay before rescheduling the task
@@ -28,9 +29,53 @@ struct Task
 
 struct InternalTask
 {
-    Task task;
+    bool execute()
+    {
+        if (!async) { return task.work(); }
+        else
+        {
+            // check if we already started the work
+            if (!fut.valid())
+            {
+                // move the work to a shared future
+                fut = std::async(std::launch::async, [work = std::move(task.work)]() {
+                          // check the result of the work
+                          std::cout << "s";
+                          bool res = work();
+                          // if the job was successful, return true, and simply remove the task
+                          if (res) { return std::make_pair(true, std::function<bool()>{}); }
+                          // if the job failed, return false and the work to be rescheduled
+                          else { return std::make_pair(false, std::move(work)); }
+                      }).share();
+            }
+            // check if the task is done
+            // give the thread some time to start
+            if (fut.wait_for(async_check_duration) == std::future_status::ready)
+            {
+                auto res = fut.get();
+                if (res.first)
 
-    std::future<void> fut;
+                {
+                    // we are done
+                    return true;
+                }
+                else
+                {
+                    // reset the future, move the original task back in, so we can reschedule it if needed
+                    task.work = std::move(res.second);
+                    fut = {};
+                }
+            }
+
+            return false;
+        }
+    }
+
+    Task           task;
+    bool           async = false;
+    Task::Duration async_check_duration{1ms};
+
+    std::shared_future<std::pair<bool, std::function<bool()>>> fut;
 };
 
 // TODO: task encapsulation of a threaded task. Should hold a task, a future that is checked on the task function
@@ -57,9 +102,34 @@ public:
         // periodically check for timed tasks. If set to 0, no periodic check will be performed and timed tasks will
         // only be handled by manually calling checkTimedTasks()
         Duration periodic_check_duration{16ms};
+        Duration async_task_check_duration{1ms}; //< wait time for async tasks to be checked for completion.
+        bool     start_immediately = true;       //< start the task engine immediately, else start() needs to be called
+
+        // monadic
+        Config& withPeriodicCheckDuration(Duration duration)
+        {
+            periodic_check_duration = duration;
+            return *this;
+        }
+
+        Config& withAsyncTaskCheckDuration(Duration duration)
+        {
+            async_task_check_duration = duration;
+            return *this;
+        }
+
+        Config& withStartImmediately(bool start)
+        {
+            start_immediately = start;
+            return *this;
+        }
     };
 
+    static Config default_config() { return Config{}; };
+
     TaskEngine(Config&& cfg = {.periodic_check_duration{16ms}});
+    void start();
+
     ~TaskEngine();
 
     // add a generic callable as a Task
@@ -90,14 +160,37 @@ public:
         else { static_assert(is_void, "Task must return bool or void"); }
     }
 
+    // add a generic callable as a AsyncTask
+    template <typename F>
+    void addAsyncTask(F&&      f,
+                      bool     reschedule_on_failure = false,
+                      Duration starting_time_offset = {},
+                      Duration reschedule_delay = {})
+    {
+        // if lambda's return type is void, we wrap it in a lambda that returns bool for convenience
+        constexpr bool is_void = std::is_same_v<decltype(f()), void>;
+        if constexpr (is_void)
+        {
+            addAsyncTask({[f = std::move(f)]() {
+                              f();
+                              return true;
+                          },
+                          reschedule_on_failure,
+                          starting_time_offset,
+                          reschedule_delay});
+        }
+        // is convertible to bool
+        else if constexpr (std::is_convertible_v<decltype(f()), bool>)
+        {
+            addAsyncTask({std::move(f), reschedule_on_failure, starting_time_offset, reschedule_delay});
+        }
+
+        else { static_assert(is_void, "Task must return bool or void"); }
+    }
+
     void addTask(Task&& task);
 
-    void addAsyncTask(Task&& task)
-    {
-        auto fut = std::async(std::launch::deferred, [task = std::move(task)]() mutable { task.execute(); });
-
-        // _async_tasks.emplace(std::move(task));
-    }
+    void addAsyncTask(Task&& task);
 
     // check for task's deadline and move them to the task queue
     void checkTimedTasks();
@@ -112,6 +205,7 @@ public:
     bool hasTimedTasks() const;
 
 private:
+    void addInternalTask(InternalTask&& task);
     void checkTimedTasks(const Task::TimePoint& time);
     void run();
 
--- pgfoundation/src/taskengine/TaskEngine.cpp
+++ pgfoundation/src/taskengine/TaskEngine.cpp
@@ -7,23 +7,22 @@ void pgf::TaskEngine::run()
     {
         std::unique_lock lk(_mutex);
         _cv.wait(lk, [this] { return _task_available || runner_thread.get_stop_token().stop_requested(); });
-
         while (!_tasks.empty())
         {
             auto internal_task = std::move(_tasks.front());
 
-            auto success = internal_task.task.execute();
+            auto success = internal_task.execute();
             if (!success && internal_task.task.reschedule_on_failure)
             {
                 _timed_tasks[std::chrono::high_resolution_clock::now() + internal_task.task.reschedule_delay] =
                     InternalTask{std::move(internal_task)};
             }
             _tasks.pop_front();
-            _task_available = false;
         }
+        _task_available = false;
 
         lk.unlock();
-        _cv.notify_one();
+        _cv.notify_all();
     }
 }
 
@@ -51,33 +50,27 @@ void pgf::TaskEngine::checkTimedTasks()
 void pgf::TaskEngine::checkTimedTasks(const Task::TimePoint& time)
 {
     // get all delayed task with deadline passed
-    const std::lock_guard lk(_mutex);
-    auto                  iter = _timed_tasks.lower_bound(time);
-    // emplace all tasks from the map to the queue
-    for (auto it = _timed_tasks.begin(); it != iter; it++)
     {
-        _tasks.emplace_back(std::move(it->second));
+        std::lock_guard lk(_mutex);
+        auto            iter = _timed_tasks.lower_bound(time);
+        // emplace all tasks from the map to the queue
+        for (auto it = _timed_tasks.begin(); it != iter; it++)
+        {
+            _tasks.emplace_back(std::move(it->second));
+        }
+        // get number of tasks that are moved to the queue
+        auto moved_tasks = std::distance(_timed_tasks.begin(), iter);
+        _timed_tasks.erase(_timed_tasks.begin(), iter);
+        _task_available = moved_tasks > 0;
+        // if there are tasks available, notify the runner thread
     }
-
-    _timed_tasks.erase(_timed_tasks.begin(), iter);
-    _task_available = true;
-    _cv.notify_one();
+    if (_task_available) { _cv.notify_all(); }
 }
 
 pgf::TaskEngine::TaskEngine(Config&& config)
   : _config(config)
 {
-    runner_thread = std::jthread{&TaskEngine::run, this};
-    if (_config.periodic_check_duration != std::chrono::high_resolution_clock::duration::zero())
-    {
-        _check_thread = std::jthread([this](std::stop_token stoken) {
-            while (!stoken.stop_requested())
-            {
-                checkTimedTasks();
-                std::this_thread::sleep_for(std::chrono::milliseconds(16));
-            }
-        });
-    }
+    if (_config.start_immediately) { start(); }
 }
 
 pgf::TaskEngine::~TaskEngine()
@@ -93,19 +86,20 @@ pgf::TaskEngine::~TaskEngine()
 
 void pgf::TaskEngine::addTask(Task&& task)
 {
+    addInternalTask(InternalTask{std::move(task)});
+}
+
+void pgf::TaskEngine::addInternalTask(InternalTask&& internal_task)
+{
     auto            now = std::chrono::high_resolution_clock::now();
     std::lock_guard lk(_mutex);
-    if (task.starting_time_offset == std::chrono::high_resolution_clock::duration::zero())
+    if (internal_task.task.starting_time_offset == std::chrono::high_resolution_clock::duration::zero())
     {
-        _tasks.emplace_back(std::move(task));
+        _tasks.emplace_back(std::move(internal_task));
         _task_available = true;
         _cv.notify_one();
     }
-    else
-    {
-        //
-        _timed_tasks[task.starting_time_offset + now] = InternalTask{std::move(task)};
-    }
+    else { _timed_tasks[internal_task.task.starting_time_offset + now] = std::move(internal_task); }
 }
 
 void pgf::TaskEngine::forceCheckTimedTasks()
@@ -128,4 +122,26 @@ bool pgf::TaskEngine::hasTimedTasks() const
 {
     std::unique_lock lk(_mutex);
     return !_timed_tasks.empty();
+}
+
+void pgf::TaskEngine::addAsyncTask(Task&& task)
+{
+    InternalTask internal_task{std::move(task), true, _config.async_task_check_duration};
+    addInternalTask(std::move(internal_task));
+}
+
+void pgf::TaskEngine::start()
+{
+    if (runner_thread.joinable()) { throw std::logic_error("TaskEngine is already running"); }
+    runner_thread = std::jthread{&TaskEngine::run, this};
+    if (_config.periodic_check_duration != std::chrono::high_resolution_clock::duration::zero())
+    {
+        _check_thread = std::jthread([this](std::stop_token stoken) {
+            while (!stoken.stop_requested())
+            {
+                checkTimedTasks();
+                std::this_thread::sleep_for(_config.periodic_check_duration);
+            }
+        });
+    }
 }
